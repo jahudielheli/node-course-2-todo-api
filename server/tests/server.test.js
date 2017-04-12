f56e1fb6cfd1602b2b95af86845b26783ccaf5dd
Jahudiel Heli Contreras Rodriguez <jahudiel.contreras@ctinter.com.mx>
2017-04-11 17:17:42 -0700

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'Primer texto todo'
 }, {
+  _id: new ObjectID(),
   text: 'Segundo texto todo'
 }];
 
@@ -64,8 +67,38 @@ describe('GET /todos', () => {
       .get('/todos')
       .expect(200)
       .expect((res) => {
-        expect(res.body.todos.length).toBe(3);
+        expect(res.body.todos.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('Debe devolver todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('debe devolver 404 si todo no lo encuentra', (done) => {
+    // asegurar que tengamos 404
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('debe devolver 404 cuando el id sea incorrecto', (done) => {
+    // /todos/123
+    request(app)
+      .get('/todos/123abc')
+      .expect(404)
       .end(done);
   });
 });
